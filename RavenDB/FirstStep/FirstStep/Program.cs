15534ba6e74a3b019d7ddc4844b89930d0e8b1e5
tsbala <bala.iyer1973@gmail.com>
2011-11-16 23:32:25 +0000

    t
    Please enter the commit message for your changes. Lines starting

---
+++ RavenDB/FirstStep/FirstStep/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Raven.Client.Document;
+
+namespace FirstStep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var documentStore = new DocumentStore {Url = "http://localhost:8080"}) 
+            {
+                documentStore.Initialize();
+                using(var documentSession = documentStore.OpenSession())
+                {
+
+                }
+            }
+        }
+    }
+}
