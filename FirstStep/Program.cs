4a0b87f450749ca2d5b095df8caaa9d4a0a194d0
Bala Iyer <bala.iyer1973@gmail.com>
2012-03-27 00:11:11 +0100

    added querying capability and ndesk options

--- FirstStep/Program.cs
+++ FirstStep/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using Raven.Client;
 using Raven.Client.Document;
 using Domain;
@@ -12,22 +9,47 @@ namespace FirstStep
     {
         static void Main(string[] args)
         {
+            bool add = false;
+            bool query = true;
+
+            new NDesk.Options.OptionSet() {
+                { "a|Add", v => add = v != null },
+                { "q|Query", v => query = v != null }
+            }; 
+
             using (var documentStore = new DocumentStore { Url = "http://localhost:8081" })
             {
                 documentStore.Initialize();
                 using (var documentSession = documentStore.OpenSession())
                 {
-                    AddStudent(documentSession, Student.Create("Ben", "Abbot", new DateTime(2000, 4, 22), Gender.Male));
-                    AddStudent(documentSession, Student.Create("Francis", "Abbot", new DateTime(2000, 5, 17), Gender.Male));
-                    AddStudent(documentSession, Student.Create("James", "Langley", new DateTime(2000, 3, 21), Gender.Male));
-                    AddStudent(documentSession, Student.Create("David", "Globe", new DateTime(2000, 4, 2), Gender.Male));
-                    AddStudent(documentSession, Student.Create("Victoria", "Green", new DateTime(2000, 6, 3), Gender.Female));
-                    AddStudent(documentSession, Student.Create("James", "Gregory", new DateTime(2000, 5, 11), Gender.Male));
-                    AddStudent(documentSession, Student.Create("Bryan", "Holmes", new DateTime(2000, 2, 1), Gender.Male));
-                    AddStudent(documentSession, Student.Create("Jake", "Whitehall", new DateTime(2000, 8, 22), Gender.Male));
-                    documentSession.SaveChanges();
+                    if (add) { AddStudents(documentSession); }
+                    if (query) { QueryStudents(documentSession); }
                 }
             }
+        }
+
+        static void AddStudents(IDocumentSession documentSession)
+        {
+            AddStudent(documentSession, Student.Create("Ben", "Abbot", new DateTime(2000, 4, 22), Gender.Male));
+            AddStudent(documentSession, Student.Create("Francis", "Abbot", new DateTime(2000, 5, 17), Gender.Male));
+            AddStudent(documentSession, Student.Create("James", "Langley", new DateTime(2000, 3, 21), Gender.Male));
+            AddStudent(documentSession, Student.Create("David", "Globe", new DateTime(2000, 4, 2), Gender.Male));
+            AddStudent(documentSession, Student.Create("Victoria", "Green", new DateTime(2000, 6, 3), Gender.Female));
+            AddStudent(documentSession, Student.Create("James", "Gregory", new DateTime(2000, 5, 11), Gender.Male));
+            AddStudent(documentSession, Student.Create("Bryan", "Holmes", new DateTime(2000, 2, 1), Gender.Male));
+            AddStudent(documentSession, Student.Create("Jake", "Whitehall", new DateTime(2000, 8, 22), Gender.Male));
+            documentSession.SaveChanges();
+        }
+
+        static void QueryStudents(IDocumentSession documentSession)
+        {
+            var students = documentSession.Query<Student>();
+            foreach (var student in students)
+	        {
+                Console.WriteLine(String.Join(" ", student.FirstName, student.LastName));
+            }
+
+            Console.ReadLine();
         }
 
         static void AddStudent(IDocumentSession documentSession, Student student)
