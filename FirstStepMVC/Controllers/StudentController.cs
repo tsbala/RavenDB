a443b0d82856303136d2a8e4753e9ffbd12a479b
Bala Iyer <bala.iyer1973@gmail.com>
2012-03-29 00:47:25 +0100

    Moar MVC and RavenDB fun with a dollop of Twitter Bootstrap

---
+++ FirstStepMVC/Controllers/StudentController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using Domain;
+
+namespace FirstStepMVC.Controllers
+{
+    public class StudentController : Controller
+    {
+        //
+        // ADD: /Student/
+        [HttpGet]
+        public ActionResult Add()
+        {
+            var student = new Student();
+            return View(student);
+        }
+
+    }
+}
